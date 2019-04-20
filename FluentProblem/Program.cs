bc5f1a7fb6762dcce4775b10879b82a15361cc2f
Patrick Kranz <patrick-kranz@live.de>
2019-04-20 23:12:23 +0200

    Add project files.

---
+++ FluentProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharedPhotos = Sample.SharedPhotos["test", 4, "male"];
+            var test = Sample.Test["Test-User", 1];
+            var halloWorld = Sample.HelloWorld;
+            Console.WriteLine(halloWorld); 
+            Console.WriteLine(sharedPhotos);
+            Console.WriteLine(test);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ FluentProblem/Sample.ftl
@@ -0,0 +1,21 @@
+﻿# Simple things are simple.
+hello-world = Hello, world!
+
+test =
+ {$userName} {$photoCount ->
+   [one] added a new photo
+  *[other] added {$photoCount} photos
+ }.
+
+# Complex things are possible.
+shared-photos =  
+    {$userName} {$photoCount ->
+        [one] added a new photo
+       *[other] added {$photoCount} new photos
+    } to {$userGender ->
+        [male] his stream 
+        [female] her stream
+       *[other] their stream
+    }.  
+
+
