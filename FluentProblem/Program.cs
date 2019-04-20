03879562f37fd236b143c2cf46505f873f3edbcd
Patrick Kranz <patrick-kranz@live.de>
2019-04-20 23:13:50 +0200

    Add comments

--- FluentProblem/Program.cs
+++ FluentProblem/Program.cs
@@ -10,9 +10,18 @@ namespace FluentProblem
     {
         static void Main(string[] args)
         {
+            // code will generated when ftl file is changed.
+            // use "go to definition" to get to the code.
+
+            // Will not work
             var sharedPhotos = Sample.SharedPhotos["test", 4, "male"];
+            
+            // will not work
             var test = Sample.Test["Test-User", 1];
+
+            // Will Work
             var halloWorld = Sample.HelloWorld;
+
             Console.WriteLine(halloWorld); 
             Console.WriteLine(sharedPhotos);
             Console.WriteLine(test);
